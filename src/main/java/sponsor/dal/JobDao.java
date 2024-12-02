b3ef0e612de4825b80f4699c7190ffc26e96f916
VivianDongChen <chen.dong1@northeastern.edu>
2024-12-01 18:45:36 -0800

    add employerName and getJobByEmployerName to JobDao

--- src/main/java/sponsor/dal/JobDao.java
+++ src/main/java/sponsor/dal/JobDao.java
@@ -1,82 +1,3 @@
-//package sponsor.dal;
-//import sponsor.model.Job;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//
-//public class JobDao {
-//    private static JobDao instance = null;
-//    protected ConnectionManager connectionManager;
-//    protected JobDao() {
-//        connectionManager = new ConnectionManager();
-//    }
-//
-//    public static JobDao getInstance() {
-//        if (instance == null) {
-//            instance = new JobDao();
-//        }
-//        return instance;
-//    }
-//
-//    public Job create(Job job) throws SQLException {
-//        String insertJob = "INSERT INTO Job(CASE_NUMBER, EMPLOYER_ID, JOB_TITLE, PW_SOC_CODE, PW_SOC_TITLE, PW_SKILL_LEVEL, PW_WAGE, WAGE_OFFER_FROM, WAGE_OFFER_TO, WAGE_OFFER_UNIT_OF_PAY) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-//        try (Connection connection = connectionManager.getConnection();
-//             PreparedStatement statement = connection.prepareStatement(insertJob)) {
-//
-//            statement.setString(1, job.getCaseNumber());
-//            statement.setInt(2, job.getEmployerId());
-//            statement.setString(3, job.getJobTitle());
-//            statement.setString(4, job.getPwSocCode());
-//            statement.setString(5, job.getPwSocTitle());
-//            statement.setString(6, job.getPwSkillLevel());
-//            statement.setDouble(7, job.getPwWage());
-//            statement.setDouble(8, job.getWageOfferFrom());
-//            statement.setDouble(9, job.getWageOfferTo());
-//            statement.setString(10, job.getWageOfferUnitOfPay());
-//            statement.executeUpdate();
-//            return job;
-//        }
-//    }
-//
-//    public Job getJobByCaseNumber(String caseNumber) throws SQLException {
-//        String selectJob = "SELECT * FROM Job WHERE CASE_NUMBER=?";
-//        try (Connection connection = connectionManager.getConnection();
-//             PreparedStatement statement = connection.prepareStatement(selectJob)) {
-//
-//            statement.setString(1, caseNumber);
-//            ResultSet results = statement.executeQuery();
-//
-//            if (results.next()) {
-//                int employerId = results.getInt("EMPLOYER_ID");
-//                String jobTitle = results.getString("JOB_TITLE");
-//                String pwSocCode = results.getString("PW_SOC_CODE");
-//                String pwSocTitle = results.getString("PW_SOC_TITLE");
-//                String pwSkillLevel = results.getString("PW_SKILL_LEVEL");
-//                double pwWage = results.getDouble("PW_WAGE");
-//                double wageOfferFrom = results.getDouble("WAGE_OFFER_FROM");
-//                double wageOfferTo = results.getDouble("WAGE_OFFER_TO");
-//                String wageOfferUnitOfPay = results.getString("WAGE_OFFER_UNIT_OF_PAY");
-//                return new Job(caseNumber, employerId, jobTitle, pwSocCode, pwSocTitle, pwSkillLevel, pwWage, wageOfferFrom, wageOfferTo, wageOfferUnitOfPay);
-//            }
-//        }
-//        return null;
-//    }
-//
-//    public Job delete(Job job) throws SQLException {
-//        String deleteJob = "DELETE FROM Job WHERE CASE_NUMBER=?";
-//        try (Connection connection = connectionManager.getConnection();
-//             PreparedStatement statement = connection.prepareStatement(deleteJob)) {
-//
-//            statement.setString(1, job.getCaseNumber());
-//            statement.executeUpdate();
-//            return null;
-//        }
-//    }
-//}
-
-
-
 package sponsor.dal;import sponsor.model.*;
 
 import java.sql.Connection;
@@ -109,7 +30,7 @@ public class JobDao {
     }
 
     public Job create(Job job) throws SQLException {
-        String insertJob = "INSERT INTO Job(CASE_NUMBER, EMPLOYER_ID, JOB_TITLE, PW_SOC_CODE, PW_SOC_TITLE, PW_SKILL_LEVEL, PW_WAGE, WAGE_OFFER_FROM, WAGE_OFFER_TO, WAGE_OFFER_UNIT_OF_PAY) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String insertJob = "INSERT INTO Job(CASE_NUMBER, EMPLOYER_ID, JOB_TITLE, PW_SOC_CODE, PW_SOC_TITLE, PW_SKILL_LEVEL, PW_WAGE, WAGE_OFFER_FROM, WAGE_OFFER_TO, WAGE_OFFER_UNIT_OF_PAY, EMPLOYER_NAME) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 //        try (Connection connection = connectionManager.getConnection();
              PreparedStatement statement = connection.prepareStatement(insertJob);//) {
 
@@ -123,6 +44,7 @@ public class JobDao {
             statement.setDouble(8, job.getWageOfferFrom());
             statement.setDouble(9, job.getWageOfferTo());
             statement.setString(10, job.getWageOfferUnitOfPay());
+            statement.setString(11, job.getEmployerName());
             statement.executeUpdate();
 
             return job;
@@ -148,8 +70,9 @@ public class JobDao {
                 double wageOfferFrom = results.getDouble("WAGE_OFFER_FROM");
                 double wageOfferTo = results.getDouble("WAGE_OFFER_TO");
                 String wageOfferUnitOfPay = results.getString("WAGE_OFFER_UNIT_OF_PAY");
+                String employerName = results.getString("EMPLOYER_NAME");
 
-                return new Job(caseNumber, employerId, jobTitle, pwSocCode, pwSocTitle, pwSkillLevel, pwWage, wageOfferFrom, wageOfferTo, wageOfferUnitOfPay);
+                return new Job(caseNumber, employerId, jobTitle, pwSocCode, pwSocTitle, pwSkillLevel, pwWage, wageOfferFrom, wageOfferTo, wageOfferUnitOfPay, employerName);
             }
 
 //        }
@@ -189,7 +112,39 @@ public class JobDao {
                     results.getDouble("PW_WAGE"),
                     results.getDouble("WAGE_OFFER_FROM"),
                     results.getDouble("WAGE_OFFER_TO"),
-                    results.getString("WAGE_OFFER_UNIT_OF_PAY")
+                    results.getString("WAGE_OFFER_UNIT_OF_PAY"),
+                    results.getString("Employer_Name")
+                );
+                jobs.add(job);
+            }
+
+//        }
+        return jobs;
+    }
+    
+    public List<Job> getJobsByEmployerName(String employerName) throws SQLException {
+        List<Job> jobs = new ArrayList<>();
+        String selectByEmployerName = "SELECT * FROM Job WHERE EMPLOYER_NAME=?";
+        
+//        try (Connection connection = connectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(selectByEmployerName);//) {
+            
+            statement.setString(1, employerName);
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()) {
+                Job job = new Job(
+                    results.getString("CASE_NUMBER"),
+                    results.getInt("EMPLOYER_ID"),
+                    results.getString("JOB_TITLE"),
+                    results.getString("PW_SOC_CODE"),
+                    results.getString("PW_SOC_TITLE"),
+                    results.getString("PW_SKILL_LEVEL"),
+                    results.getDouble("PW_WAGE"),
+                    results.getDouble("WAGE_OFFER_FROM"),
+                    results.getDouble("WAGE_OFFER_TO"),
+                    results.getString("WAGE_OFFER_UNIT_OF_PAY"),
+                    results.getString("EMPLOYER_NAME")
                 );
                 jobs.add(job);
             }
